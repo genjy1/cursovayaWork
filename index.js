0fa08084b31d2d42fa84e86b97079e9d30d8f2b4
genjy1 <idontknower1997@gmail.com>
2023-10-12 20:43:41 +0300

    feat:added responsive header

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const doc = window.document;
+const links = doc.querySelectorAll(".link"); // любой нужный селектор
+const linksCount = links.length;
+const currentURL = doc.URL;
+const item = doc.querySelectorAll('.list_item')
+
+for (let i = 0; i < linksCount; i++) {
+    let linkURL = links[i].href;
+
+  // linkURL == currentURL, если не надо светить уровни выше
+    if (currentURL.startsWith(linkURL)) {
+    links[i].classList.toggle("active-link");
+    item[i].classList.toggle('active-link')
+}
+}
+
