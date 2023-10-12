36e6465bd65b45f098d365b00aca39659418d4bb
genjy1 <idontknower1997@gmail.com>
2023-10-12 23:40:58 +0300

    refactor:updated link to esnhub discord

--- script/genBanner.js
+++ script/genBanner.js
@@ -2,5 +2,5 @@ export const createBanner = () => {
     console.log('banner');
 
     const header = document.body.querySelector('header');
-    header.insertAdjacentHTML('afterend','<a href="#" class="link"><div class="banner"><div class="banner__container container"><span class="italic">Присоединяйся</span><br>К команде <span class="italic underline">авторов</span> ESNHub</div></div></a>')
+    header.insertAdjacentHTML('afterend','<a href="https://discord.gg/ZdnPZuZN" class="link"><div class="banner"><div class="banner__container container"><span class="italic">Присоединяйся</span><br>К команде <span class="italic underline">авторов</span> ESNHub</div></div></a>')
 }
