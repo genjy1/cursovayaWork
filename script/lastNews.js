f79a778e526d22336e1b942f71f6fc7629399026
genjy1 <idontknower1997@gmail.com>
2023-11-28 17:05:43 +0300

    feat:added recent news block rendering

--- script/lastNews.js
+++ script/lastNews.js
@@ -9,7 +9,16 @@ const recentNewsLink = recentNews.querySelector('.recent-news_link');
 const newsImg = recentNews.querySelector('.news-img');
 const newsHeader = recentNews.querySelector('.news-header');
 
+if (window.outerWidth < 426) {
+    newsImg.style.width = '380px';
+    recentNews.style.marginTop = '39px'
+}else {
+    newsImg.style.width = '1360px';
+    recentNews.style.marginTop = '68px'
+}
+
+
 recentNewsLink.href = `article.html?news=true&id=${recentNewsData.id}`
 newsImg.src = `${recentNewsData.img}`;
-newsImg.style.width = '1360px';
+
 newsHeader.textContent = `${recentNewsData.name}`
